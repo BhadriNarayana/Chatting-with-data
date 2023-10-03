2a1eea356f0b8b4dc860f768ce715fb6e879f1fe
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-10-03 12:11:42 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title="Chat with pdf data", page_icon=":books:")
+
+    st.header("Chat with your pdfs! :books:")
+
+    st.text_input()
+    
+
+
+if __name__ == '__main__':
+    main()
